0e61cb2d8e154aaa1495818fd0714785c1b08f2c
gasseklopper <m-morley@gmx.de>
2018-10-07 11:47:13 +0200

    git hub rep url changes

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 3000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `https://gasseklopper.github.io/restaurant/data/restaurants.json`;
     //return `http://127.0.0.1:${port}/data/restaurants.json`;
   }
 
